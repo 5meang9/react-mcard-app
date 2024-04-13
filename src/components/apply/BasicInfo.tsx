43cc3657b37dd7107e420c50161b563ff000cfa2
5meang9 <uiop3724@daum.net>
2024-04-13 21:17:12 +0900

    BasicInfo 1. 레이아웃 작성 2. 연소득, 신용점수, 결제일 등 select 세부사항 처리 하는 로직 진행중

--- src/components/apply/BasicInfo.tsx
+++ src/components/apply/BasicInfo.tsx
@@ -1,8 +1,63 @@
-// 카드 공통 정보
+import { ChangeEvent, useCallback, useState } from "react"
 
-function BasicInfo(){
+import { 결제일옵션, 신용점수옵션, 연소득옵션 } from "@/constants/apply"
+import Select from "../shared/Select"
+import { ApplyValues } from "@/models/apply"
+import Spacing from "../shared/Spacing"
+import FixedBottomButton from "../shared/FixedBottomButton"
+
+type InfoValues = Pick<ApplyValues, 'salary' | 'creditScore' | 'payDate'>
+
+// 카드 공통 정보
+function BasicInfo({onNext}: { onNext: (infoValues: InfoValues) => void }){
+  const [infoValues, setInfoValues] = useState<InfoValues>({
+    salary: '',
+    creditScore: '',
+    payDate: '',
+  })
+
+  const handleInfoChange = useCallback((e: ChangeEvent<HTMLSelectElement>) => {
+    setInfoValues((prevValues) => ({
+      ...prevValues,
+      [e.target.name]: e.target.value
+    }))
+  }, [])
+
+  const 모든정보가선택되었는가 = Object.values(infoValues).every(
+    (value) => value,
+  )
+
   return(
-    <div>eee</div>
+    <div>
+      <Select 
+        name='salary'
+        label="연소득" 
+        options={연소득옵션}
+        placeholder={연소득옵션[0].label} 
+        value={infoValues.salary}
+        onChange={handleInfoChange}
+      />
+      <Spacing size={16} />
+      <Select 
+        name='creditScore'
+        label="신용점수" 
+        options={신용점수옵션} 
+        placeholder={신용점수옵션[0].label} 
+        value={infoValues.creditScore}
+        onChange={handleInfoChange}
+      />
+      <Spacing size={16} />
+      <Select 
+        name='payDate'
+        label="결제일" 
+        options={결제일옵션} 
+        placeholder={결제일옵션[0].label} 
+        value={infoValues.payDate} 
+        onChange={handleInfoChange}
+      />
+
+      <FixedBottomButton label="다음" onClick={() => onNext(infoValues)} disabled={모든정보가선택되었는가 === false} />
+    </div>
   )
 }
 
