75513211e6239d1385ca4cfae8a77d9422877809
won <iop3724@naver.com>
2024-05-10 10:26:40 +0900

    카드발급 완료 페이지 css 수정

--- src/pages/ApplyDone.tsx
+++ src/pages/ApplyDone.tsx
@@ -3,6 +3,7 @@ import { parse } from 'qs'
 import Flex from '@shared/Flex'
 import Text from '@shared/Text'
 import FixedBottomButton from '@shared/FixedBottomButton'
+import Spacing from '@/components/shared/Spacing'
  
 function ApplyDone() {
   const { success } = parse(window.location.search, {
@@ -10,20 +11,23 @@ function ApplyDone() {
   }) as { success: string }
 
   return (
-    <Flex>
-      <Text>
-        {success === 'true'
-          ? '카드가 발급되었습니다.'
-          : '카드 발급에 실패했습니다.'}
-      </Text>
+    <>
+      <Spacing size={50} />
+      <Flex justify="center">
+        <Text textAlign='center'>
+          {success === 'true'
+            ? '카드가 발급되었습니다.'
+            : '카드 발급에 실패했습니다.'}
+        </Text>
 
-      <FixedBottomButton
-        label="확인"
-        onClick={() => {
-          window.history.back()
-        }}
-      />
-    </Flex>
+        <FixedBottomButton
+          label="확인"
+          onClick={() => {
+            window.history.back()
+          }}
+        />
+      </Flex>
+    </>
   )
 }
 
