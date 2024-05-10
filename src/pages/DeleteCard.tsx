a85b3358801824f82bab42b6e760d6887ffcf6b5
won <iop3724@naver.com>
2024-05-10 10:02:49 +0900

    apply card delete test

--- src/pages/DeleteCard.tsx
+++ src/pages/DeleteCard.tsx
@@ -2,7 +2,7 @@ import { COLLECTIONS } from "@/constants";
 import useUser from "@/hooks/auth/useUser";
 import { getAppliedCard } from "@/remote/apply";
 import { store } from "@/remote/firebase";
-import { addDoc, collection, getDoc, getDocs } from "firebase/firestore";
+import { addDoc, collection, deleteDoc, doc, getDoc, getDocs } from "firebase/firestore";
 import { useQuery } from "react-query";
 import { useParams } from "react-router-dom"
 
@@ -16,7 +16,10 @@ function DeleteCardPage(){
   )
 
   const appliedId = data ? data.id : null; // data가 존재할 때에만 id를 가져옵니다.
-
+  
+  if (appliedId) {
+    deleteDoc(doc(store, COLLECTIONS.CARD_APPLY, appliedId))
+  }
 
   return(
     // <CardPage isDelete={true} />
